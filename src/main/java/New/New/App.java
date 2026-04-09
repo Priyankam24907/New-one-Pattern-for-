c733b0318f50ea03bac7e9e77ab768eaddb7712f
Priyanka <testerway88@gmail.com>
2026-04-09 17:24:26 +0530

    new file for practice

---
+++ src/main/java/New/New/App.java
@@ -0,0 +1,13 @@
+package New.New;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/project/Newone.java
@@ -0,0 +1,16 @@
+package com.project;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Newone {
+	
+	public static void main(String[]args) {
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://study.monetanalytics.com/survey_builder/#/survey/dashboard");
+		
+	}
+
+}
